c5ad9f3588672f2e37782761664ea2c94bdde67d
jasonlama09 <jasonl467@nycstudents.net>
2025-12-22 14:19:51 +0000

    save practical

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,4 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function front(){
+    document.getElementById("highlight").innerHTML = "image container";
+}
